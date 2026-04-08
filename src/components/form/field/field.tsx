0ce2011dc23154379de2ec8c72cad3392149ab3e
Clement Grihon <gclems@gmail.com>
2026-04-08 16:04:21 +0200

    feat: add label props to Field, remove input max width

--- src/components/form/field/field.tsx
+++ src/components/form/field/field.tsx
@@ -1,21 +1,21 @@
+import type { ComponentProps } from "react";
+
 import { Field as BaseField } from "@base-ui/react";
 
 import { InputDescription } from "../../form/input-description/input-description";
 import { InputError } from "../../form/input-error/input-error";
 import { Label } from "../../form/label/label";
-
 import "./field.css";
 
 function Field({
 	label,
-	required,
 	error,
 	description,
 	orientation = "vertical",
 	children,
-}: {
+	...labelProps
+}: ComponentProps<typeof Label> & {
 	label?: React.ReactNode;
-	required?: boolean;
 	error?: React.ReactNode;
 	description?: React.ReactNode;
 	children?: React.ReactNode;
@@ -24,7 +24,7 @@ function Field({
 	return (
 		<BaseField.Root data-slot="field-root" data-orientation={orientation}>
 			<div data-slot="field-label">
-				<Label render={<BaseField.Label />} required={required}>
+				<Label render={<BaseField.Label />} {...labelProps}>
 					{label}
 				</Label>
 
