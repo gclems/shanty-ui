4ab22dc355f4648806ec6c0647ce604b26c777b5
Clement Grihon <gclems@gmail.com>
2026-04-08 15:06:25 +0200

    feat: card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,34 @@
+import type { ComponentProps } from "react";
+
+import "./card.css";
+
+function Root(props: ComponentProps<"div"> & ComponentProps<typeof Header>) {
+	return <div data-slot="card" {...props} />;
+}
+
+function Header({
+	title,
+	description,
+}: {
+	title?: React.ReactNode;
+	description?: React.ReactNode;
+}) {
+	return (
+		<div data-slot="card-header">
+			{!!title && <h2 data-slot="card-title">{title}</h2>}
+			{!!description && <p data-slot="card-description">{description}</p>}
+		</div>
+	);
+}
+
+function Body(props: ComponentProps<"div">) {
+	return <div data-slot="card-body" {...props} />;
+}
+
+function Footer(props: ComponentProps<"div">) {
+	return <div data-slot="card-footer" {...props} />;
+}
+
+const Card = Object.assign(Root, { Header, Body, Footer });
+
+export { Card };
