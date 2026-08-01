6b65524a8259ba96301de3ac6b06e1b536ff2ff4
Gclems <gclems@gmail.com>
2026-08-01 21:14:01 +0200

    fix: scroll area correct width handling when vertical only

--- src/components/scroll-area/scroll-area.tsx
+++ src/components/scroll-area/scroll-area.tsx
@@ -2,40 +2,43 @@ import { ScrollArea as BaseScrollArea } from "@base-ui/react/scroll-area";
 import "./scroll-area.css";
 
 function Scrollbar(props: BaseScrollArea.Scrollbar.Props) {
-  return (
-    <BaseScrollArea.Scrollbar data-slot="scroll-bar" {...props}>
-      <BaseScrollArea.Thumb data-slot="scroll-bar-thumb" />
-    </BaseScrollArea.Scrollbar>
-  );
+	return (
+		<BaseScrollArea.Scrollbar data-slot="scroll-bar" {...props}>
+			<BaseScrollArea.Thumb data-slot="scroll-bar-thumb" />
+		</BaseScrollArea.Scrollbar>
+	);
 }
 
 function ScrollArea({
-  vertical = false,
-  horizontal = false,
-  children,
-  ...props
+	vertical = false,
+	horizontal = false,
+	children,
+	...props
 }: BaseScrollArea.Root.Props & {
-  vertical?: boolean;
-  horizontal?: boolean;
+	vertical?: boolean;
+	horizontal?: boolean;
 }) {
-  return (
-    <BaseScrollArea.Root data-slot="scroll-area" {...props}>
-      <BaseScrollArea.Viewport data-slot="scroll-area-viewport">
-        <BaseScrollArea.Content
-          data-slot="scroll-area-content"
-          data-vertical={vertical || undefined}
-          data-horizontal={horizontal || undefined}
-        >
-          {children}
-        </BaseScrollArea.Content>
-      </BaseScrollArea.Viewport>
-      {vertical && <Scrollbar orientation="vertical" />}
-      {horizontal && <Scrollbar orientation="horizontal" />}
-      {vertical && horizontal && (
-        <BaseScrollArea.Corner data-slot="scroll-area-corner" />
-      )}
-    </BaseScrollArea.Root>
-  );
+	return (
+		<BaseScrollArea.Root data-slot="scroll-area" {...props}>
+			<BaseScrollArea.Viewport
+				data-slot="scroll-area-viewport"
+				data-horizontal={horizontal || undefined}
+			>
+				<BaseScrollArea.Content
+					data-slot="scroll-area-content"
+					data-vertical={vertical || undefined}
+					data-horizontal={horizontal || undefined}
+				>
+					{children}
+				</BaseScrollArea.Content>
+			</BaseScrollArea.Viewport>
+			{vertical && <Scrollbar orientation="vertical" />}
+			{horizontal && <Scrollbar orientation="horizontal" />}
+			{vertical && horizontal && (
+				<BaseScrollArea.Corner data-slot="scroll-area-corner" />
+			)}
+		</BaseScrollArea.Root>
+	);
 }
 
 export { ScrollArea };
